95fa596c07eb6f9172e6e733055f15bb16d1c2d4
pr-citrate <pr.citrate@gmail.com>
2024-08-12 02:27:23 +0900

    Feature: Add StarStream

---
+++ packages/star-stream-core/src/index.ts
@@ -0,0 +1,27 @@
+import { mean, zScore } from "../utils/statistics";
+
+type range = [number, number];
+
+export function StarStream(k: number, targetRange: range) {
+    return class Star {
+        private readonly _k: number;
+        private readonly _range: range;
+
+        constructor() {
+            this._k = k;
+            this._range = targetRange;
+        }
+
+        public readStar(ratings: number[], value: number): number {
+            const z = zScore(ratings, value);
+            const t = z * this._k + mean(this._range);
+            return this.restrictValue(t);
+        }
+
+        private restrictValue(value: number): number {
+            return value > this._range[1] ? this._range[1] : value < this._range[0] ? this._range[0] : value;
+        }
+    };
+}
+
+type StarMaker = ReturnType<typeof StarStream>;
