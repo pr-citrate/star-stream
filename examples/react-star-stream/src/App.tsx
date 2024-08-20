3a851785ac8a60325798ef9f868b9e9d060d7cc0
pr-citrate <123797964+pr-citrate@users.noreply.github.com>
2024-08-20 10:10:52 +0900

    Update App.tsx

--- examples/react-star-stream/src/App.tsx
+++ examples/react-star-stream/src/App.tsx
@@ -1,64 +1,33 @@
-import React, { useState } from 'react';
-import { StarStream } from '@star-stream/core';
-import StarChart from './StarChart';
+import React from 'react';
 import './App.css';
+import StarChart from './StarChart';
+import { useStarStream } from '../../../packages/react-star-stream/src/hooks/useStarStream';
 
-const App: React.FC = () => {
-    const [starStream] = useState(new StarStream(1, [0, 10]));
-    const [newStar, setNewStar] = useState<number>(0);
-    const [stars, setStars] = useState<number[]>(starStream.stars);
-    const [sum, setSum] = useState<number>(starStream.sum);
-    const [mean, setMean] = useState<number>(starStream.mean);
-    const [stdDev, setStdDev] = useState<number>(starStream.stdDev);
+function App() {
+  const {
+    stars,
+    mean,
+    stdDev,
+    addStar,
+    removeStar,
+    reAlignStar,
+  } = useStarStream(100, [0, 100]);
 
-    const addStar = () => {
-        if (newStar >= 0) {
-            starStream.addStar(newStar);
-            updateStats();
-        }
-    };
-
-    const removeStar = (index: number) => {
-        starStream.removeStar(index);
-        updateStats();
-    };
-
-    const updateStats = () => {
-        setStars(starStream.stars);
-        setSum(starStream.sum);
-        setMean(starStream.mean);
-        setStdDev(starStream.stdDev);
-    };
-
-    return (
-        <div className="App">
-            <h1>Star Rating System</h1>
-            <input
-                type="number"
-                value={newStar}
-                onChange={(e) => setNewStar(Number(e.target.value))}
-                min="0"
-                max="10"
-            />
-            <button onClick={addStar}>Add Star</button>
-
-            <ul>
-                {stars.map((star, index) => (
-                    <li key={index}>
-                        {star} <button onClick={() => removeStar(index)}>Remove</button>
-                    </li>
-                ))}
-            </ul>
-
-            <div>
-                <p>Total Sum: {sum}</p>
-                <p>Mean: {mean.toFixed(2)}</p>
-                <p>Standard Deviation: {stdDev.toFixed(2)}</p>
-            </div>
-
-            <StarChart stars={stars} mean={mean} stdDev={stdDev} />
-        </div>
-    );
-};
+  return (
+    <div className="App">
+      <h1>Star Stream Example</h1>
+      <StarChart stars={stars} />
+      <div>
+        <button onClick={() => addStar(Math.random() * 100)}>Add Star</button>
+        <button onClick={() => removeStar(stars.length - 1)}>Remove Star</button>
+        <button onClick={() => reAlignStar([50, 75])}>Re-align Stars</button>
+      </div>
+      <div>
+        <p>Mean: {mean}</p>
+        <p>Standard Deviation: {stdDev}</p>
+      </div>
+    </div>
+  );
+}
 
 export default App;
